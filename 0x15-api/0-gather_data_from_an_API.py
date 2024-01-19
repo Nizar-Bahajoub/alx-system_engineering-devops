0ce0af729ede4498a5d705391416c0a4cff50b91
nizar <n.bahajoub@gmail.com>
2024-01-19 11:09:37 -0800

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+from requests import *
+from sys import argv
+
+
+user = get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+user_tasks = get("https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])).json()
+done_tasks = [task for task in user_tasks if task["completed"]]
+all_tasks = get("https://jsonplaceholder.typicode.com/todos").json()
+
+print("Employee {} is done with done with tasks({}/{})".format(user.get("name"), len(done_tasks), len(user_tasks)))
+
+for i in done_tasks:
+    print("\t {}".format(i["title"]))

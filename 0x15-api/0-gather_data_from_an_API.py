c0c59c88759e8196e68e7591817a6fd125e67af5
nizar <n.bahajoub@gmail.com>
2024-01-19 13:01:15 -0800

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     done_tasks = [task for task in user_tasks if task["completed"]]
     all_tasks = get("https://jsonplaceholder.typicode.com/todos").json()
 
-    print("Employee {} is done with done with tasks({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
             user.get("name"), len(done_tasks), len(user_tasks)))
 
     for i in done_tasks:

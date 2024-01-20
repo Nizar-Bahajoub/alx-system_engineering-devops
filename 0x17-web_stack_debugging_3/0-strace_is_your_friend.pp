9e0e2a1e88f9cfc6b1f6e71a1ada810236106bd4
nizar <n.bahajoub@gmail.com>
2024-01-20 04:32:27 -0800

    Bug Fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Replacing 'phpp' with 'php' extantion in 'wp-settings.php'
 
 exec { 'fixing-error':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }

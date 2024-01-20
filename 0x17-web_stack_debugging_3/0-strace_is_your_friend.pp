929f7884991d0587b60f25b6fa5ea70bbe87ea3b
nizar <n.bahajoub@gmail.com>
2024-01-20 04:01:08 -0800

    Gub Fixed

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Replacing 'phpp' with 'php' extantion in 'wp-settings.php'
+
+exec { 'fixing-error':
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
+	path    => '/usr/local/bin/:/bin/'
+}

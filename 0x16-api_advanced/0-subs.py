50c81e6c20238c42d8e093e00fc38ebd76825212
nizar <n.bahajoub@gmail.com>
2024-01-30 03:07:46 -0800

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Return the number of subscribers on a given subreddit """
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
-    headers = {
-            "User-Agent": "Linux:0x16.api.advanced:v1.0.0"
-            }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get("https://www.reddit.com/r/{}/about.json".format(
+                            subreddit),
+                            headers={"User-Agent": "Nizar-Custom-User-Agent"},
+                            allow_redirects=False)
+    if response.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    return response.json().get("data").get("subscribers")

30d39a7087d04f4f7b0d3dae3e6aa0dda1ca344e
nizar <n.bahajoub@gmail.com>
2024-01-30 04:40:32 -0800

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,9 @@ headers = {"User-Agent": "AlxCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed for a given subreddit"""
+    """ prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
